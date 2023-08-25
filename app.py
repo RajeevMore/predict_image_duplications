901bf6fc940754b1aaadd5ce440c5e8aa933a197
Rajeev More <100182072+RajeevMore@users.noreply.github.com>
2023-08-25 20:51:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
 #from typing import Annotated
 import uvicorn
 from code import finddedup
-
 import os
-#from app import app
-import urllib.request
-#from flask import Flask, flash, request, redirect, url_for, render_template
-#from werkzeug.utils import secure_filename
-
-# import pandas as pd
-# pip install python-multipart
-
 from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
-
 
 @app.post("/document-dedup")
 async def create_upload_file(file: UploadFile):
@@ -28,13 +18,11 @@ async def create_upload_file(file: UploadFile):
         return { "isDocDuplicate": False }
     elif file.filename in Duplicate:
         folder_path = "uploaded_files/"
-        file_name = file.filename  # Replace with the name of the file you want to delete
+        file_name = file.filename  
 
         file_path = os.path.join(folder_path, file_name)
         os.remove(file_path)
         return{"isDocDuplicate": True }
-
-    #return {"info": f"file '{file.filename}' saved at '{file_location}'"}
 
 
 if __name__ == '__main__':
